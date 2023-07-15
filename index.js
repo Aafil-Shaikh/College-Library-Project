53443e2a1bc1ee4c850ec04d1c6cb9c7ad96ff16
Caeldan Rodrigues <caeldanrodrigues01@gmail.com>
2023-07-15 11:33:11 +0530

    linked hompage route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("mongodb");
 // const BooksDAO = require("./models/dao/booksDAO.js");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -14,25 +15,39 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-MongoClient.connect(process.env.uri, {
-  maxPoolSize: 50,
-  wtimeoutMS: 2500,
-  useNewUrlParser: true
-})
-  .catch(err => {
-    console.error(err.stack);
-    process.exit(1);
-  })
-  .then(async client => {
-    // await BooksDAO.injectDB(client);
+// MongoClient.connect(process.env.uri, {
+//   maxPoolSize: 50,
+//   wtimeoutMS: 2500,
+//   useNewUrlParser: true
+// })
+//   .catch(err => {
+//     console.error(err.stack);
+//     process.exit(1);
+//   })
+//   .then(async client => {
+//     // await BooksDAO.injectDB(client);
 
-    app.get("/", (req, res) => {
-      res.send("hello");
-    });
+//     app.get("/", (req, res) => {
+//       res.send("hello");
+//     });
 
-    //add route here
+//     //add route here
 
-    app.listen(port, () => {
-      console.log(`Server listening on port ${port}`);
-    });
-  });
+//     app.listen(port, () => {
+//       console.log(`Server listening on port ${port}`);
+//     });
+//   });
+
+mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "database connection error!"));
+db.once("open", () => {
+  console.log("database connected");
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, console.log(`server live on port ${port}`));
